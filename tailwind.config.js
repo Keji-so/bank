3a3c9f23f21f88194c18e82dd59860f4a0a38464
kejiojo <ojokeji@gmail.com>
2021-03-13 09:19:20 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        yell: '#E2AC0F',
+        blu: '#082C66',
+        bac: '#333333',
+        grey: '#C4C4C4',
+      },
+      fontSize: {
+        'tiny': '10px',
+        'smol': '12px',
+        'mid': '14px',
+        'lar': '16px',
+    },
+      fontFamily: {
+        body: ['Roboto']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
